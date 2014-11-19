88487788a9c1ba388af902718ea64040617a6e87
Travis Herrick <quasimonomial@gmail.com>
2014-11-19 10:32:38 -0800

    Added the ability to edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,17 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy!
+    redirect_to articles_url
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+    render :edit
+  end
+
   def index
     @articles = Article.all
     render :index
@@ -20,6 +31,16 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_url(@article)
+    else
+      flash[:errors] = @article.errors.full_messages
+      redirect_to edit_article_url(@article)
+    end
   end
 
   private
